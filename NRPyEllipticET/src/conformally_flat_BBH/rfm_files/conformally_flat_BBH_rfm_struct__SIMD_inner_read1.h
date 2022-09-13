eaf4ee8afacea99a2b631f8babc0b26134a9f027
Leo Werneck <leow155@gmail.com>
2022-09-13 11:42:08 -0700

    NRPyEllipticET/src/conformally_flat_BBH: removed SIMD files (for now)

--- NRPyEllipticET/src/conformally_flat_BBH/rfm_files/conformally_flat_BBH_rfm_struct__SIMD_inner_read1.h
+++
@@ -1,3 +0,0 @@
-CCTK_ATTRIBUTE_UNUSED const REAL_SIMD_ARRAY f1_of_xx1 = ReadSIMD(&rfmstruct->f1_of_xx1[i1]);
-CCTK_ATTRIBUTE_UNUSED const REAL_SIMD_ARRAY f1_of_xx1__D1 = ReadSIMD(&rfmstruct->f1_of_xx1__D1[i1]);
-CCTK_ATTRIBUTE_UNUSED const REAL_SIMD_ARRAY f1_of_xx1__DD11 = ReadSIMD(&rfmstruct->f1_of_xx1__DD11[i1]);
--- NRPyEllipticET/src/conformally_flat_BBH/rfm_files/conformally_flat_BBH_rfm_struct__SIMD_outer_read1.h
+++
@@ -1,3 +0,0 @@
-CCTK_ATTRIBUTE_UNUSED const double NOSIMDf1_of_xx1 = rfmstruct->f1_of_xx1[i1]; CCTK_ATTRIBUTE_UNUSED const REAL_SIMD_ARRAY f1_of_xx1 = ConstSIMD(NOSIMDf1_of_xx1);
-CCTK_ATTRIBUTE_UNUSED const double NOSIMDf1_of_xx1__D1 = rfmstruct->f1_of_xx1__D1[i1]; CCTK_ATTRIBUTE_UNUSED const REAL_SIMD_ARRAY f1_of_xx1__D1 = ConstSIMD(NOSIMDf1_of_xx1__D1);
-CCTK_ATTRIBUTE_UNUSED const double NOSIMDf1_of_xx1__DD11 = rfmstruct->f1_of_xx1__DD11[i1]; CCTK_ATTRIBUTE_UNUSED const REAL_SIMD_ARRAY f1_of_xx1__DD11 = ConstSIMD(NOSIMDf1_of_xx1__DD11);
